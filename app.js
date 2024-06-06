f6b9c729c53c5a17ba369c15a70856778f6b1397
WEB DEVELOPEMENT with Hunaina <161623039+hunainatanveer78@users.noreply.github.com>
2024-06-06 14:24:26 +0500

    hunaina tanveer developer

---
+++ app.js
@@ -0,0 +1,37 @@
+document.write('Result: <br> The value of a is:10 <br> .......................................<br> <br>')
+document.write('The value of ++a is: 11 <br> Now the value of a is: 11 <br> <br>')
+document.write('The value of a++ is: 11 <br> Now the value of a is: 12 <br> <br>')
+document.write('The value of --a is: 11 <br> Now the value of a is: 11 <br> <br>')
+document.write('The value of a-- is: 11 <br> Now the value of a is: 10 <b> <br> <b> <br><b> <br>')
+
+
+// a = 'a is 1'
+//  console.log(a);
+//  b = 'b is 2'
+//  console.log(b);
+//  value = 'result is'
+
+//  console.log(value);
+//  var a = 1
+//  var b = 2
+// var value =
+//  console.log(a+b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
